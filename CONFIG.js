8a76688be3ff25d5a51cb4de247a6c5c775d5b82
Ian Cullinane <ian.cullinane@gmail.com>
2017-02-10 12:20:18 -0500

    update with config

---
+++ CONFIG.js
@@ -0,0 +1,30 @@
+var config = {};
+
+config.name = "basic";
+config.header = "Ian Cullinane";
+config.layout = {
+    user: {
+        title: "Basic",
+        name: "Ian Cullinane",
+        contact: "ian.cullinane@gmail.com",
+        about: "I like stuff.",
+    },
+    history: [
+        {
+            company: "Turbine Games",
+            title: "Cloud Solutions & DevOps Engineer",
+            date: "June, 2015 - present",
+            description: "Lorem ipsum dolor sit amet, consectetur adipiscing elit. Ut fermentum dictum accumsan. Vestibulum accumsan, ipsum quis viverra porta, massa mauris eleifend nisi, sit amet cursus quam ligula in odio. Mauris eget ex eros. Vestibulum at arcu in dui rhoncus lacinia sit amet sed ex. Curabitur malesuada porta tempus. Nam id dictum augue, iaculis consequat lorem."
+        },
+        {
+            company: "Whole Foods Market",
+            title: "Signmaker / Graphic Artist",
+            date: "August, 2009 - August, 2012",
+            description: "Lorem ipsum dolor sit amet, consectetur adipiscing elit. Ut fermentum dictum accumsan. Vestibulum accumsan, ipsum quis viverra porta, massa mauris eleifend nisi, sit amet cursus quam ligula in odio. Mauris eget ex eros. Vestibulum at arcu in dui rhoncus lacinia sit amet sed ex. Curabitur malesuada porta tempus. Nam id dictum augue, iaculis consequat lorem."
+        }
+    ]
+};
+config.default_stuff =  ['red','green','blue','apple','yellow','orange','politics'];
+config.port = 3333;
+
+module.exports = config;
